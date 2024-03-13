6338f452a1b2eb4b5e2aef73672f5489798d9b35
carloshcardoso <33209995+carloshcardoso@users.noreply.github.com>
2024-03-12 23:24:30 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, url_quote
 import db
 
 app = Flask(__name__)
